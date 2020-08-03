f454d5c40550b1a7628f714f9f4921086b263c38
Tetsuro Higuchi <higuchi.tetsuro@gmail.com>
2020-08-03 23:22:43 +0900

    improve performance

--- 5.longest_palindoromic_substring/src/lib.rs
+++ 5.longest_palindoromic_substring/src/lib.rs
@@ -2,30 +2,41 @@ pub struct Solution;
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        let mut max:usize = 0;
-        let mut palindrome = String::from("");
-        for (i, _) in s.chars().enumerate() {
-            for j in (i + 1)..=s.len() {
-                let chars: Vec<char> = s.chars().collect();
-                let slice = &chars[i..j];
-                if Solution::is_palidrome(slice) && max < slice.len() {
+        if s.len() == 0 {
+            return s;
+        }
+        let chars: Vec<char> = s.chars().collect();
+        let mut max: usize = 0;
+        let mut palindrome = &chars[..1];
+        for (start, c) in chars.iter().enumerate() {
+            let mut end = start + 1;
+            while let Some(n) = Solution::find_index(&chars, c, end) {
+                let slice = &chars[start..=n];
+                if max < slice.len() && Solution::is_palindrome(slice) {
                     max = slice.len();
-                    palindrome = slice.iter().cloned().collect();
+                    palindrome = slice;
                 }
+                end = n + 1;
             }
         }
-        println!("{}", max);
-        palindrome
+        palindrome.iter().clone().collect::<String>()
     }
 
-    pub fn is_palidrome(chars: &[char]) -> bool {
-        let length = chars.len();
-        for (i, c) in chars.into_iter().enumerate() {
-            if c != &chars[length - i - 1] {
-                return false
+    pub fn is_palindrome(chars: &[char]) -> bool {
+        for (i, c) in chars.into_iter().rev().enumerate() {
+            if c != &chars[i] {
+                return false;
             }
         }
         true
+    }
+
+    pub fn find_index(chars: &[char], ch: &char, offset: usize) -> Option<usize> {
+        chars
+            .iter()
+            .skip(offset)
+            .position(|c| c == ch)
+            .map(|s| s + offset)
     }
 }
 
@@ -35,16 +46,48 @@ mod tests {
 
     #[test]
     fn test1() {
-        assert_eq!(Solution::longest_palindrome("babad".to_string()), "bab".to_string());
+        assert_eq!(
+            Solution::longest_palindrome("babad".to_string()),
+            "bab".to_string()
+        );
     }
 
     #[test]
     fn test2() {
-        assert_eq!(Solution::longest_palindrome("cbbd".to_string()), "bb".to_string());
+        assert_eq!(
+            Solution::longest_palindrome("cbbd".to_string()),
+            "bb".to_string()
+        );
     }
     #[test]
     fn test3() {
-        let p = "tembwxtvddsttolegryohdlhxhycymqybzfzcbkzdwcekzfapmpfyeivfoeeqoqdhylziqpnyzuzeeutpqpalbddlliuehvkhqevgjdkskvphidcjmpcmetzwqkzcnxjcjywhfzplntbkuddmbcovearburjqyirbladcrhfkfdfgsmyhdsfmmxmslwkymkgaguilxghmfgaldcogtfnbqakctqtqakupwrxkmbjpmzqngwldmaugzizgwmediyzxevspxdwruyzrmnhchtxlgtb".to_string();
-        assert_eq!(Solution::longest_palindrome(p), "bb".to_string());
+        let p = "jhgtrclvzumufurdemsogfkpzcwgyepdwucnxrsubrxadnenhvjyglxnhowncsubvdtftccomjufwhjupcuuvelblcdnuchuppqpcujernplvmombpdttfjowcujvxknzbwmdedjydxvwykbbamfnsyzcozlixdgoliddoejurusnrcdbqkfdxsoxxzlhgyiprujvvwgqlzredkwahexewlnvqcwfyahjpeiucnhsdhnxtgizgpqphunlgikogmsffexaeftzhblpdxrxgsmeascmqngmwbotycbjmwrngemxpfakrwcdndanouyhnnrygvntrhcuxgvpgjafijlrewfhqrguwhdepwlxvrakyqgstoyruyzohlvvdhvqmzdsnbtlwctetwyrhhktkhhobsojiyuydknvtxmjewvssegrtmshxuvzcbrabntjqulxkjazrsgbpqnrsxqflvbvzywzetrmoydodrrhnhdzlajzvnkrcylkfmsdode".to_string();
+        assert_eq!(Solution::longest_palindrome(p), "hhktkhh".to_string());
+    }
+    #[test]
+    fn test4() {
+        let p = "a".to_string();
+        assert_eq!(Solution::longest_palindrome(p), "a".to_string());
+    }
+    #[test]
+    fn test5() {
+        let p = "".to_string();
+        assert_eq!(Solution::longest_palindrome(p), "".to_string());
+    }
+
+    #[test]
+    fn test_find1() {
+        assert_eq!(
+            Solution::find_index(&("cbcde".chars().collect::<Vec<char>>()), &'c', 1),
+            Some(2)
+        )
+    }
+
+    #[test]
+    fn test_find2() {
+        assert_eq!(
+            Solution::find_index(&("cbcde".chars().collect::<Vec<char>>()), &'f', 2),
+            None
+        )
     }
 }
--- 5.longest_palindoromic_substring/src/main.rs
+++ 5.longest_palindoromic_substring/src/main.rs
@@ -1,6 +1,6 @@
 use longest_palindoromic_substring::Solution;
 
 fn main() {
-    let b = Solution::is_palidrome(&['a', 'b', 'c', 'b', 'a']);
+    let b = Solution::is_palindrome(&['a', 'd', 'b', 'b', 'a']);
     println!("{}", b);
 }
